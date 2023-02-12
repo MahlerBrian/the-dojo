f4d1e0a5a55d716e9d59ed63ddea4b24eb855799
Brian Albert <brian.david.albert@gmail.com>
2023-02-12 09:49:36 -0500

    added login/logout functionality

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const { dispatch, user } = useAuthContext()
   
   const logout = async () => {
     setError(null)
@@ -14,6 +14,9 @@ export const useLogout = () => {
 
     try {
       // sign the user out
+      //change logged in status BEFORE logging our or request to change status will be denied
+      const { uid } = user
+      await projectFirestore.collection('users').doc(uid).update({ online: false })
       await projectAuth.signOut()
       
       // dispatch logout action
